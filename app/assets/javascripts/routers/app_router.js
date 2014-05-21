bcf25d6beae68eb79d89a95da548e0ae4aa31e33
zhyliana <zhyliana@gmail.com>
2014-05-21 00:40:51 -0700

    router swap view and initialize w root

--- app/assets/javascripts/routers/app_router.js
+++ app/assets/javascripts/routers/app_router.js
@@ -1,13 +1,26 @@
 FP.Routers.AppRouter = Backbone.Router.extend({
+  initialize: function(options){
+    this.$rootEl = options.$rootEl  
+  },
+  
   routes: {
     "" : "drawingNew"
   },
   
   drawingNew: function(){
+    var newDrawing = new FP.Models.Drawing();
     var newDrawingView = new FP.Views.NewDrawing({
       model: newDrawing
     });
     
     this._swapView(newDrawingView);
   },
+  
+  _swapView: function(view) {
+    if (this.currentView) {
+      this.currentView.remove();
+    }
+    
+    this.$rootEl.html(view.render().$el)
+  }
 })
