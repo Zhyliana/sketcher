c1bee7edb0145b6577f86e09fba672a185fd7c1e
zhyliana <zhyliana@gmail.com>
2014-05-22 10:13:19 -0700

    adds users show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "root#root"
-  
+  resources: :users, only: [:show]
   namespace :api, defaults: { format: :json } do
     resources :drawings
   end
