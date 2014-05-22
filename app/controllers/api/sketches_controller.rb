57da0fea63547155eb2a5e80fad31104e0c138a3
zhyliana <zhyliana@gmail.com>
2014-05-22 14:22:38 -0700

    creates sketches controller

--- app/controllers/api/sketches_controller.rb
+++ app/controllers/api/sketches_controller.rb
@@ -1,13 +1,27 @@
-class SketchesController < ApplicationController
-  def create
-    @sketch = Sketch.new(params[:post])
-    @sketch.user_id = current_user.id
+module Api
+  class SketchesController < ApiController
+    def index
+      @sketches = current_user.sketches.all
+      render :index
+    end
 
-    if @sketch.save
-      render :json => @sketch
-    else
-      render :json =>  @sketch.errors.full_messages, :status => 422
+    def show
+      @sketch = Sketch.find(params[:id])
+      render partial: "api/sketches/sketch", locals: { sketch: @sketch }
+    end
+
+    def create
+      @sketch = current_user.sketches.build(sketch_params)
+      if @sketch.save
+        render partial: "api/sketches/sketch", locals: { sketch: @sketch }
+      else
+        render json: { errors: @sketch.errors.full_messages }, status: 422
+      end
+    end
+
+    private
+    def sketch_params
+      params.require(:sketch).permit(:user_id, :data_url, :votes)
     end
   end
-
 end
