29e574de4693a182d967b4a58698c6b34fc36de2
zhyliana <zhyliana@gmail.com>
2014-05-23 09:38:33 -0700

    adds whitecard model and controller

--- app/assets/javascripts/models/sketches.js
+++ app/assets/javascripts/models/sketches.js
@@ -1,3 +1,6 @@
 SketchMate.Models.Sketch = Backbone.Model.extend({
   urlRoot: "/api/sketches",
+  is_image: function () {
+      return (this.get("description") ? false : true)
+    }
 })
---
+++ test/controllers/white_cards_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WhiteCardsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
