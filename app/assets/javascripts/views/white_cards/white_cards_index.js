a51cba2ea23664d1f223266259712001d2d22ab1
zhyliana <zhyliana@gmail.com>
2014-05-24 15:32:53 -0700

    cards index: first draft

--- app/assets/javascripts/views/white_cards/white_cards_index.js
+++ app/assets/javascripts/views/white_cards/white_cards_index.js
@@ -1,3 +1,31 @@
-SketchMate.Views.IndexWhiteCard = Backbone.CompositeView.extend({
+SketchMate.Views.IndexWhiteCards = Backbone.CompositeView.extend({
+  model: SketchMate.Models.WhiteCard, 
+  template: JST["white_cards/index"],
   
+  initialize: function(options){
+    this.listenTo(this.collection, "sync",  this.render);
+    this.listenTo(this.collection, "add", this.addCardView);
+    
+    var view = this;
+    
+    this.collection.each(function(card){
+      view.addCardView(card);
+    });
+  },
+  
+  addCardView: function(card){
+    var cardShowView = new SketchMate.Views.ShowWhiteCard({ model: card });
+    this.addSubview("#cards", cardShowView)
+  },
+  
+  render: function(){
+    var renderedContent = this.template({
+      cards: this.collection
+    });
+      
+    this.$el.html(renderedContent);   
+    this.attachSubviews();   
+    return this;
+  },
+   
 })
