ca324a48d9726e8bfa4d6353f241ffe485c22888
zhyliana <zhyliana@gmail.com>
2014-05-26 14:55:16 -0700

    messing w sql

--- app/controllers/api/sketches_controller.rb
+++ app/controllers/api/sketches_controller.rb
@@ -2,19 +2,21 @@ module Api
   class SketchesController < ApiController
     wrap_parameters :sketch, include: [:image, :votes]
     def index
-      @sketches = current_user.sketches.includes(:white_cards).all
-      
+      # @sketches = current_user.sketches.includes(:white_cards).all
+      @sketches = Sketch.all
+      fail
       render :index
     end
+    
+    def playable
+      
+    end
+
 
     def show
       @sketch = Sketch.find(params[:id])
 
       render partial: "api/sketches/sketch", locals: { sketch: @sketch }
-    end
-    
-    def playable
-   
     end
 
     def create
@@ -28,7 +30,7 @@ module Api
 
     private
     def sketch_params
-      params.require(:sketch).permit(:user_id, :image, :votes, :user, :cards)
+      params.require(:sketch).permit(:user_id, :image, :user, :playable?, :white_cards)
     end
   end
 end
