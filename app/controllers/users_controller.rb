bf45f8421919459c90d4655dcadcf605ff7ebea8
zhyliana <zhyliana@gmail.com>
2014-05-29 14:42:13 -0700

    resolve in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,4 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  def new
-=======
-  def show
-    @user = User.find(params[:id])
-=======
-  # def show
-  #   @user = User.find(params[:id])
-  # end
-  
-  def index
-    @users = User.all
-    fail 
-    respond_to do |format|
-      format.html { render :index }
-      format.json { render :json => @users.to_json }
-    end
->>>>>>> safe-email2
-=======
   def show
     @user = User.find(params[:id])
 
@@ -30,17 +9,11 @@ class UsersController < ApplicationController
     @users = User.all
 
     render "api/users/index", locals: { users: @users }
->>>>>>> fcce62a45134c218fcbf709cf270261744513159
   end
 
 
   private  
   def user_params
-<<<<<<< HEAD
-    params.require(:user).permit(:email)
->>>>>>> safe-email
-=======
     params.require(:user).permit(:email, :sketches, :white_cards)
->>>>>>> fcce62a45134c218fcbf709cf270261744513159
   end
 end
